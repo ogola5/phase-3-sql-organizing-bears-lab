e220777c6a43f41484f6648176b64ede84b157eb
ogola5 <ogolaevance5@gmail.com>
2022-11-03 18:21:54 +0300

    completed assignment

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,9 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 5, "Male", "Black", "Slow", 0);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 7, "Male", "Chocolate", "Calm", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 9, "Female", "Grey", "Happy", 0);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 8, "Male", "Blue", "Withdrwan", 0);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 10, "Girl", "Black", "Slaty", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 4, "Male", "Green", "Religious", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 6, "Female", "Brown", "Aggressive", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 7, "GIRL", "Beige", "Calm", 1);
+
+INSERT INTO bears (name, sex) VALUES ("Mr. Chocolate", "M");
+INSERT INTO bears (name, sex) VALUES ("Rowdy", "M");
+INSERT INTO bears (name, sex) VALUES ("Tabitha", "F");
+INSERT INTO bears (name, sex) VALUES ("Sergeant Brown", "M");
+INSERT INTO bears (name, sex) VALUES ("Melissa", "F");
+INSERT INTO bears (name, sex) VALUES ("Grinch", "M");
+INSERT INTO bears (name, sex) VALUES ("Wendy", "M");
+INSERT INTO bears (name, sex) VALUES ( NULL, NULL );
--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -1,24 +1,24 @@
 def selects_all_female_bears_return_name_and_age
   
-  "SELECT name,age FROM bears WHERE gender ='F'";
+  "SELECT name,age FROM bears WHERE sex ='F';"
 end
 
 def selects_all_bears_names_and_orders_in_alphabetical_order
   
-  "SELECT bears.name FROM bears ORDER BY name";
+  "SELECT name FROM bears ORDER BY name;"
 end
 
 def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
  
-  "SELECT bears.name,bears.age FROM bears WHERE alive =1 ORDER BY age";
+  "SELECT name,age FROM bears WHERE alive =true ORDER BY age;"
 end
 
 def selects_oldest_bear_and_returns_name_and_age
  
-  "SELECT Bears.name,bears.age FROM bears ORDER BY age LIMIT 1"
+  "SELECT bears.name,bears.age FROM bears ORDER BY bears.age DESC LIMIT 1;"
 end
 
 def select_youngest_bear_and_returns_name_and_age
   
-  "SELECT bears.name,bears.age FROM bears ORDER BY age LIMIT 1"
+  "SELECT bears.name,bears.age FROM bears ORDER BY bears.age ASC LIMIT 1;"
 end
